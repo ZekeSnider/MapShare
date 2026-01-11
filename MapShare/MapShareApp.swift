e779d5fc6d37ce01921646bd11ff9ddb91610d94
Zeke Snider <zekesnider@me.com>
2026-01-10 17:35:25 -0800

    Initial Commit

---
+++ MapShare/MapShareApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapShareApp.swift
+//  MapShare
+//
+//  Created by Zeke Snider on 1/10/26.
+//
+
+import SwiftUI
+
+@main
+struct MapShareApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
